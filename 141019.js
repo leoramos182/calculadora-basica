c814ed83915f594b29f138a6d8b0964f17adb3a9
Léo Ramos Andrade <leo.ramos182@hotmail.com>
2019-10-14 16:51:47 -0300

    Add files via upload

---
+++ 141019.js
@@ -0,0 +1,33 @@
+function start(){
+
+}
+function somar(){
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 + num2 ;
+    var resultado = parseFloat(resultado.toFixed(2));
+    document.getElementById('resultado').setAttribute("value",resultado);
+    
+}
+function subtracao(){
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var resultado = num1 - num2 ;
+    var resultado = parseFloat(resultado.toFixed(2));
+    document.getElementById('resultado').setAttribute("value",resultado)
+
+}
+function divisao(){
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var resultado = num1 / num2 ;
+    var resultadoArredondado = parseFloat(resultado.toFixed(2))
+    document.getElementById('resultado').setAttribute("value",resultadoArredondado);
+}
+function multiplicacao(){
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var resultado = num1 * num2 ;
+    var resultado = parseFloat(resultado.toFixed(2));
+    document.getElementById('resultado').setAttribute("value",resultado)
+}
