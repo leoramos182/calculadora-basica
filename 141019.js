359bc670bd24dd935adaa5a7f5e620021293fa36
leoramos182 <leo.ramos182@hotmail.com>
2019-10-14 23:06:55 -0300

    Adicionado a função numeroPrimo

--- 141019.js
+++ 141019.js
@@ -31,3 +31,22 @@ function raizQuadrada(){
     var raizQuadrada = Math.sqrt(raizQuadrada)
     document.getElementById('raiz').setAttribute("value",raizQuadrada)
 }
+function primo(){
+    var numeroPrimo = parseFloat(document.getElementById('resultado').value)
+    var i = 0
+    var auxiliar = 0
+    for(i = 0; i <= numeroPrimo;i++){
+        if(numeroPrimo % i == 0){
+            auxiliar = auxiliar + 1
+        }
+
+    }
+    for(i=0;i<=numeroPrimo;i++){
+        if(auxiliar > 2){
+            document.getElementById('primo').setAttribute("value","Número não é primo!")
+        }
+        else{
+            document.getElementById('primo').setAttribute("value","Número é primo!")
+        }
+    }
+}
